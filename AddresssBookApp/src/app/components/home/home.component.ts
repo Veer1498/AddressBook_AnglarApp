637a887f0ff4ef069cf611b1a82a2e84ec5b3469
Veer1498 <singavarapuveerendra@gmail.com>
2022-11-16 02:51:29 +0530

    [Veer] Added : Ability to Call the HTTP GET and POST API's from Backend, and Modified UI to adapt that data retrieved

--- AddresssBookApp/src/app/components/home/home.component.ts
+++ AddresssBookApp/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AddressBookService } from 'src/app/services/address-book.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(
+    private service:AddressBookService
+  ) { }
+  list:any;
   ngOnInit(): void {
+    this.service.getList().subscribe((data:any) =>{
+      this.list = data.data;
+    })
   }
 
 }
